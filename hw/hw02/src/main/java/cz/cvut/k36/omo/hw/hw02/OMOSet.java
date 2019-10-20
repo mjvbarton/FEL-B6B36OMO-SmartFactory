264b7ed29a36aa230044d98f8591620bb34a32d0
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-20 17:12:12 +0200

    HW02 - OMOSet.add, .remove, .contains, .toArray - WIP

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSet.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSet.java
@@ -1,9 +1,41 @@
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.HashSet;
+
 /**
  *
  * @author Matej
  */
 public class OMOSet extends OMOSetBase implements OMOSetView{
+    private final HashSet<Integer> set = new HashSet();
+    
+    @Override
+    public void add(int element) {
+        set.add(element);
+    }
+
+    @Override
+    public void remove(int element) {
+        set.remove(element);
+    }
+
+    @Override
+    public boolean contains(int element) {
+        return set.contains(element);
+    }
+
+    @Override
+    public int[] toArray() {
+        int[] array = new int[]{};
+        for(Integer element : set){
+            array.
+        }
+    }
+
+    @Override
+    public OMOSetView copy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     // metody rozhraní OMOSetView a OMOSetBase
+    
 }
