ca9d7b8900c5c60883c69265ca92eb3243b9f827
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-21 12:30:54 +0200

    HW02 - Implemented class OMOSetUnion

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetUnion.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetUnion.java
@@ -5,27 +5,40 @@ package cz.cvut.k36.omo.hw.hw02;
  * @author Matej
  */
 public class OMOSetUnion implements OMOSetView{
+    protected OMOSetView setA;
+    protected OMOSetView setB;
+    
     /**
      * Creates set union from set A and set B given.
      * @param setA a <b>REFERENCE</b> to set A
      * @param setB a <b>REFERENCE</b> to set B
      */
     OMOSetUnion(OMOSetView setA, OMOSetView setB){
-        
+        this.setA = setA;
+        this.setB = setB;
     }
-
+    
     @Override
     public boolean contains(int element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return setA.contains(element) || setB.contains(element);
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] arrA = setA.toArray();
+        int[] arrB = setB.toArray();
+        int[] result = new int[arrA.length + arrB.length];
+        for(int i = 0; i < arrA.length; i++){
+            result[i] = arrA[i];
+        }
+        for(int i = 0; i < arrB.length; i++){
+            result[arrA.length + i] = arrB.length;
+        }
+        return result;
     }
 
     @Override
     public OMOSetView copy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new OMOSetUnion(setA.copy(), setB.copy());
     }
 }
