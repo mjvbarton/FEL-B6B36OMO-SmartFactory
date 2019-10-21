eef1c84bcefa9e71f1048f40be5f32b6d0bb140a
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-21 12:31:13 +0200

    HW02 - Implemented class OMOSetEven

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetEven.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetEven.java
@@ -1,30 +1,45 @@
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matej
  */
 // třída reprezentující množinu sudých čísel
 public class OMOSetEven implements OMOSetView{
+    private final OMOSetView setA;
+    
     /**
      * Creates new OMOSetEven from given set
      * @param setA a <b>REFERENCE</b> to some set
      */
     OMOSetEven(OMOSetView setA){
+        this.setA = setA;
     }
 
     @Override
     public boolean contains(int element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return setA.contains(element) && element % 2 == 0;
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> rawResult = new ArrayList();
+        for (Integer element : setA.toArray()) {
+            if (element % 2 == 0) {
+                rawResult.add(element);
+            }
+        }
+        int[] result = new int[rawResult.size()];
+        for (int i = 0; i < rawResult.size(); i++) {
+            result[i] = rawResult.get(i);
+        }
+        return result;
     }
 
     @Override
     public OMOSetView copy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new OMOSetEven(setA.copy());
     }
 }
