084240b0b62d85ec2647881d69c4b78704e32d7b
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-21 12:31:22 +0200

    HW02 - Implemented class OMOSetComplement

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetComplement.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetComplement.java
@@ -1,11 +1,16 @@
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matej
  */
 // třída reprezentující A\B: doplněk množiny B vzhledem k množině A:  A\B = { x | x?A ? x?B }
 public class OMOSetComplement implements OMOSetView{
+    protected OMOSetView setA;
+    protected OMOSetView setB;
+    
     /**
      * Creates new complement set to setA and set B given.
      * It performs this set operation {@code setA - setB}
@@ -13,23 +18,32 @@ public class OMOSetComplement implements OMOSetView{
      * @param setB a <b>REFERENCE</b> to set B
      */
     OMOSetComplement(OMOSetView setA, OMOSetView setB){
-        
+        this.setA = setA;
+        this.setB = setB;
     }
-
-    // metody rozhrani OMOSetView
 
     @Override
     public boolean contains(int element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return setA.contains(element) && !setB.contains(element);
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> rawResult = new ArrayList();
+        for(Integer element : setA.toArray()){
+            if(setB.contains(element) == false){
+                rawResult.add(element);
+            }
+        }
+        int[] result = new int[rawResult.size()];
+        for(int i = 0; i < rawResult.size(); i++){
+            result[i] = rawResult.get(i);
+        }
+        return result;  
     }
 
     @Override
     public OMOSetView copy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new OMOSetComplement(setA.copy(), setB.copy());
     }
 }
