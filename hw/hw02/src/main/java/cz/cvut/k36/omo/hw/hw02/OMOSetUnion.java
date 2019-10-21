5257ebb61e610da05be68ee57b79f83d4dc61c0a
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-21 12:51:07 +0200

    HW02 - Modified OMOSetUnion.toArray(), changed implementation to be similar as e. g. in OMOSetComplement.toArray()

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetUnion.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetUnion.java
@@ -1,5 +1,7 @@
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matej
@@ -25,20 +27,19 @@ public class OMOSetUnion implements OMOSetView{
 
     @Override
     public int[] toArray() {
-        OMOSetIntersection intersect = new OMOSetIntersection(setA, setB);
-        OMOSetComplement compl = new OMOSetComplement(setA, setB);
-        int[] arrIntersect = intersect.toArray();
-        int[] arrCompl = compl.toArray();
-        int[] result = new int[arrIntersect.length + arrCompl.length];
-        
-        for(int i = 0; i < arrIntersect.length; i++){
-            result[i] = arrIntersect[i];
+        ArrayList<Integer> rawResult = new ArrayList();
+        for(Integer element : setA.toArray()){
+            rawResult.add(element);
         }
-        
-        for(int i = 0; i < arrCompl.length; i++){
-            result[arrIntersect.length + i] = arrCompl[i];
+        for(Integer element : setB.toArray()){
+            if(setA.contains(element) == false){
+                rawResult.add(element);
+            }
         }
-        
+        int[] result = new int[rawResult.size()];
+        for(int i = 0; i < rawResult.size(); i++){
+            result[i] = rawResult.get(i);
+        }
         return result;
     }
 
