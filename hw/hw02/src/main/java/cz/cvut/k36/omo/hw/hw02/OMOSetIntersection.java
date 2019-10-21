0e5572bc096f3862c08a7332ac4bde03e7f036a2
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-10-21 12:31:04 +0200

    HW02 - Implemented class OMOSetIntersection

--- hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetIntersection.java
+++ hw/hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSetIntersection.java
@@ -1,31 +1,47 @@
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Matej
  */
 public class OMOSetIntersection implements OMOSetView{
+    protected final OMOSetView setA;
+    protected final OMOSetView setB;
+    
     /**
      * Creates intersection from set A and set B given.
      * @param setA a <b>REFERENCE</b> to set A
      * @param setB a <b>REFERENCE</b> to set B
      */
     OMOSetIntersection(OMOSetView setA, OMOSetView setB){
-        
+        this.setA = setA;
+        this.setB = setB;
     }
 
     @Override
     public boolean contains(int element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return setA.contains(element) && setB.contains(element);
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Integer> rawResult = new ArrayList();
+        for(Integer element : setA.toArray()){
+            if(setB.contains(element)){
+                rawResult.add(element);
+            }
+        }
+        int[] result = new int[rawResult.size()];
+        for(int i = 0; i < rawResult.size(); i++){
+            result[i] = rawResult.get(i);
+        }
+        return result;                
     }
 
     @Override
     public OMOSetView copy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new OMOSetIntersection(setA.copy(), setB.copy());
     }
 }
