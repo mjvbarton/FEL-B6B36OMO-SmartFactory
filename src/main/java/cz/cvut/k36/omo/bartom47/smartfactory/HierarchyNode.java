c1315f4838b959bb8a81411e7297ea5f08545deb
Matěj Bartoň <bartom47@fel.cvut.cz>
2020-01-08 19:52:34 +0100

    Finalizing the abstract class.

--- src/main/java/cz/cvut/k36/omo/bartom47/smartfactory/HierarchyNode.java
+++ src/main/java/cz/cvut/k36/omo/bartom47/smartfactory/HierarchyNode.java
@@ -1,18 +1,33 @@
 package cz.cvut.k36.omo.bartom47.smartfactory;
 
+import cz.cvut.k36.omo.bartom47.smartfactory.consumables.configuration.ConfigurationData;
+import cz.cvut.k36.omo.bartom47.smartfactory.consumables.configuration.ConfigurationDataContainer;
+import cz.cvut.k36.omo.bartom47.smartfactory.consumables.consumption.ConsumptionData;
+import cz.cvut.k36.omo.bartom47.smartfactory.consumables.consumption.ConsumptionDataContainer;
 import cz.cvut.k36.omo.bartom47.smartfactory.events.Event;
 import cz.cvut.k36.omo.bartom47.smartfactory.events.EventPropagator;
 import cz.cvut.k36.omo.bartom47.smartfactory.events.PropagatableEvent;
+import java.util.List;
 
 /**
  *
  * @author Matej
+ * @param <K> type of the configuration model node
+ * @param <T> type of the consumption model node
  */
-public abstract class HierarchyNode implements EventPropagator{
-
+public abstract class HierarchyNode<K extends ConfigurationData, T extends ConsumptionData> implements EventPropagator, 
+        ConfigurationDataContainer<K>, 
+            ConsumptionDataContainer<T>{    
+    
+    /**
+     * Get list of children nodes events are propagated to.
+     * @return list of children nodes for propagator
+     */
+    protected abstract List<HierarchyNode> getPropagatorChildren();
+       
     @Override
     public void propagate(PropagatableEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        getPropagatorChildren().forEach(childrenNode -> childrenNode.handle(e));
     }
 
     @Override
